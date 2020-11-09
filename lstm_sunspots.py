64ce217bce2f11f8ffaaaba31814822d730bdca0
ziippy <ziippy@naver.com>
2020-11-09 14:08:50 +0900

    lstm_sunspots.py

---
+++ lstm_sunspots.py
@@ -0,0 +1,75 @@
+# https://storage.googleapi.com/laurencemoroney-blog.appspot.com/Sunspots.csv
+
+import csv
+time_step = []
+sunspots = []
+
+with open('/home/ziippy/dl_data/Sunspots.csv') as csvfile:
+    reader = csv.reader(csvfile, delimiter=',')
+    next(reader)
+    for row in reader:
+        sunspots.append(float(row[2]))
+        time_step.append(int(row[0]))
+
+print(time_step[:10])
+print(sunspots[:10])
+
+import numpy as np
+series = np.array(sunspots)
+time = np.array(time_step)
+
+import matplotlib.pyplot as plt
+def plot_series(time, series, format="-", start=0, end=None):
+    plt.plot(time[start:end], series[start:end], format)
+    plt.xlabel("Time")
+    plt.ylabel("Value")
+    plt.grid(True)
+    plt.show()
+
+# plot_series(time, series)
+
+split_time = 1000
+time_train = time[:split_time]
+x_train = series[:split_time]
+time_valid = time[split_time:]
+x_valid = series[split_time:]
+
+window_size = 20
+batch_size = 32
+shuffle_buffer_size = 1000
+
+import tensorflow as tf
+def windowed_dataset(series, window_size, batch_size, shuffle_buffer):
+  dataset = tf.data.Dataset.from_tensor_slices(series)
+  dataset = dataset.window(window_size + 1, shift=1, drop_remainder=True)
+  dataset = dataset.flat_map(lambda window: window.batch(window_size + 1))
+  dataset = dataset.shuffle(shuffle_buffer).map(lambda window: (window[:-1], window[-1]))
+  dataset = dataset.batch(batch_size).prefetch(1)
+  return dataset
+
+tf.keras.backend.clear_session()
+dataset = windowed_dataset(x_train, window_size, batch_size, shuffle_buffer_size)
+
+model = tf.keras.models.Sequential([
+  tf.keras.layers.Dense(10, input_shape=[window_size], activation='relu'),
+  tf.keras.layers.Dense(10, activation='relu'),
+  tf.keras.layers.Dense(1)
+])
+
+model.compile(loss="mse", optimizer=tf.keras.optimizers.SGD(lr=1e-6, momentum=0.9))
+history = model.fit(dataset,epochs=100,verbose=1)
+
+########################
+
+forecast = []
+results = []
+for time in range(len(series) - window_size):
+  forecast.append(model.predict(series[time:time + window_size][np.newaxis]))
+
+forecast = forecast[split_time-window_size:]
+results = np.array(forecast)[:, 0, 0]
+
+plt.figure(figsize=(10, 6))
+
+plot_series(time_valid, x_valid)
+plot_series(time_valid, results)
